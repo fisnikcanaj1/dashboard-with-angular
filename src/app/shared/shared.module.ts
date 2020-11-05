4a51ede10940b633e00f314441b2a206bae703cc
Fisnik <fisnik-canaj-test@gmail.com>
2020-11-05 18:33:41 +0100

    header added

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,34 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FooterComponent} from "./components/footer/footer.component";
+import {HeaderComponent} from "./components/header/header.component";
+import {SidebarComponent} from "./components/sidebar/sidebar.component";
+import {MatDividerModule} from '@angular/material/divider';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from '@angular/material/icon';
+import {FlexLayoutModule} from "@angular/flex-layout";
+
+@NgModule({
+    declarations: [
+        FooterComponent,
+        HeaderComponent,
+        SidebarComponent
+    ],
+    imports: [
+        CommonModule,
+        MatDividerModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatButtonModule,
+        FlexLayoutModule,
+        MatIconModule,
+    ],
+    exports: [
+        HeaderComponent,
+        FooterComponent,
+        SidebarComponent
+    ]
+})
+export class SharedModule {
+}
